1e675ff24cf652e71cb1374ccbf19c15f79f822d
Aya Yahia <ayayahia01@gmail.com>
2024-01-20 21:23:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import Home from "./Components/Home";
+import { Route, Routes } from "react-router-dom";
+import Products from "./Components/Products";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="row">
+        <div className="col-2  Sidebar">
+          <Sidebar />
+        </div>
+        <div className="col-10">
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="/products" element={<Products/>} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Sidebar() {
+    return (
+        <ul>
+            <li>
+<Link href="#"> get all product  </Link>
+            </li>
+            <li>
+            <Link href="#"> get all categories  </Link> 
+            </li>
+        </ul>
+    )
+}
+export default Sidebar ; 
